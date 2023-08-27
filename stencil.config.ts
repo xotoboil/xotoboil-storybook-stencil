dcb6e8231fcdf078da03d007fae4ce96a4e42125
Alberto <42894888+Nenino2@users.noreply.github.com>
2023-08-27 15:44:57 +0200

    feature: added exports

--- stencil.config.ts
+++ stencil.config.ts
@@ -1,5 +1,9 @@
 import { Config } from '@stencil/core';
 import { sass } from '@stencil/sass';
+import { vueOutputTarget } from "@stencil/vue-output-target";
+import { angularOutputTarget } from "@stencil/angular-output-target";
+import { reactOutputTarget } from "@stencil/react-output-target";
+
 
 export const config: Config = {
   namespace: 'xotoboil-storybook-stencil',
@@ -8,6 +12,20 @@ export const config: Config = {
     { type: 'dist-custom-elements', },
     { type: 'docs-readme', },
     { type: 'www', serviceWorker: null },
+    vueOutputTarget({
+      componentCorePackage: "xotoboil-storybook-stencil",
+      proxiesFile: "./lib/vue/components.ts",
+    }),
+    angularOutputTarget({
+      componentCorePackage: "xotoboil-storybook-stencil",
+      directivesProxyFile: "./lib/angular/components.ts",
+      directivesArrayFile: "./lib/angular/directives.ts",
+    }),
+    reactOutputTarget({
+      componentCorePackage: "xotoboil-storybook-stencil",
+      proxiesFile: "./lib/react/index.ts",
+      includeDefineCustomElements: true,
+    }),
   ],
   plugins: [
     sass({
